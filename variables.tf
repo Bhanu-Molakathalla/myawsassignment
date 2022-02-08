7a8f9500a58a8875f3f6abea9d757e185aa23ceb
Bhanu-Molakathalla <bhanuprakash.javaventures@gmail.com>
2022-02-08 16:09:21 -0500

    Updated Port

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "app_image" {
 
 variable "app_port" {
   description = "Port exposed by the docker image to redirect traffic to"
-  default     = 8080
+  default     = 3000
 }
 
 variable "app_count" {
